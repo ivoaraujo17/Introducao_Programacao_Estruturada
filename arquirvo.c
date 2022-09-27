e529388976f9e34fba08085d5d9541e07cecebd2
Ivo.araujo <86474505+ivoaraujo17@users.noreply.github.com>
2022-09-27 13:49:26 -0300

    Teste

--- arquirvo.c
+++ arquirvo.c
@@ -1,2 +1,13 @@
+# include <stdio.h>
 
+int main(){
+    float n1, n2, n3, media;
+    char nome[20];
+    printf("Informe o nome do aluno: ");
+    scanf("%f", &nome);
+    printf("Informe as 3 notas do aluno, apertando enter a cada nota: ");
+    scanf("%f %f %f", &n1,&n2,&n3);
+    media = (n1 + n2 + n3)/3;
+    printf("A media do aluno %c é %.2f", nome, media);
+}
 
